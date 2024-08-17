7c0107b0af027c374c36f34a9873ada01daa19a6
Philippe BARDON <phbardon@me.com>
2024-08-17 15:35:57 +0200

    Update Package.swift
    Affind from: "1.0.0"

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["Parma"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/phbardon/Down")
+        .package(url: "https://github.com/phbardon/Down", from: "1.0.0")
     ],
     targets: [
         .target(
